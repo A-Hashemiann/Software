9e665cb03bc3abcee920f6055444502b48422c77
A-Hashemiann <amen.hashemian@gmail.com>
2023-10-14 19:57:31 +0200

    Modele_Setup

--- Web_Dev/Node.js/Local_Module/Module_01.js
+++ Web_Dev/Node.js/Local_Module/Module_01.js
@@ -3,11 +3,28 @@
 const Module_01={
 
 Static_Func: function(){
+    let x=0
     console.log("hello world");
+    console.log(this.Var_01)
+    return ++x;
 },
 
 
 Dynammic_Func : new Date().toLocaleDateString(),
 
 
-}
+Var_01: 0,
+
+Func_01: ()=>{
+ 
+ return this.Var_01++
+
+},
+
+
+
+};
+
+
+
+module.exports=  Module_01
---
+++ Web_Dev/Node.js/Local_Module/app.js
@@ -0,0 +1,12 @@
+
+//require loacal module 
+const LocMod =require('./Module_01.js');
+
+
+
+//LocMod.Func_01;
+
+
+console.log(LocMod.Var_01)
+console.log(LocMod.Dynammic_Func)
+console.log(LocMod.Func_01)
