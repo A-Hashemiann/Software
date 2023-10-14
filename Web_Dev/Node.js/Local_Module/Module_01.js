30a9144b5e83597e9dfcebfcf0db6c2b47431cf7
A-Hashemiann <amen.hashemian@gmail.com>
2023-10-14 17:35:50 +0200

    Modele_Setup

---
+++ Web_Dev/Node.js/Local_Module/Module_01.js
@@ -0,0 +1,13 @@
+
+//create basic js objects with dynamic and static varible 
+const Module_01={
+
+Static_Func: function(){
+    console.log("hello world");
+},
+
+
+Dynammic_Func : new Date().toLocaleDateString(),
+
+
+}
