88b546945c98afe7d50d02649691dda7f84a6c0e
A-Hashemiann <amen.hashemian@gmail.com>
2023-10-27 21:56:35 +0200

    setup

---
+++ Web_Dev/Node.js/Server/Random_Server.js
@@ -0,0 +1,8 @@
+//this code will create servers on random ports within the specified range at regular time intervals and the previous server will be closed before starting a new one the first server is also started immediately
+
+const http = require('http');
+
+const minPort = 3000; // define the range of ports you want to start from (e.g., starting from 3000)
+const maxPort = 4000; // up to a certain port
+
+let currentServer = null;
