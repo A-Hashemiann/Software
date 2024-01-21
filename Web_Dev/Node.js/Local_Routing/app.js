5277625979b844667994f0b17066787d8574df90
Amin Hashemian <143293364+A-Hashemiann@users.noreply.github.com>
2024-01-21 19:23:19 +0100

    Delete Web_Dev/Node.js directory

--- Web_Dev/Node.js/Local_Routing/app.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-const displayContent = require("./routes");
-
-
-//create server and run module function on server
-const server = http.createServer(displayContent);
-
-server.listen(3001);
