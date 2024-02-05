252e78c08cd8e2bac4e53dc25063300864c3a700
Amin Hashemian <143293364+A-Hashemiann@users.noreply.github.com>
2024-02-05 15:23:05 +0100

    Update main.py

--- Science_Dev/Python/PyLatex/Doc_Generation/main.py
+++ Science_Dev/Python/PyLatex/Doc_Generation/main.py
@@ -1,1 +1,4 @@
+#Imports: The script imports necessary classes and functions from the PyLaTeX library
+from pylatex import Command, Document, Section, Subsection
+from pylatex.utils import NoEscape, italic
 
