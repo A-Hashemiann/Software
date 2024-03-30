bbf2656f26c1b565e97e9c32ade8ee8612e35d8b
A-Hashemiann <amen.hashemian@gmail.com>
2024-03-30 20:59:23 +0100

    sizeof

---
+++ Embedded_Dev/C++/Basics/data_type_sizes.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //size of fundamental data types
+    cout << "Size of char: " << sizeof(char) << " bytes" << endl;
+    cout << "Size of int: " << sizeof(int) << " bytes" << endl;
+
+
+    return 0;
+}
