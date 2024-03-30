2ff7176d4b12d105a66641f0a29d8fcbdda6116e
A-Hashemiann <amen.hashemian@gmail.com>
2024-03-30 21:34:34 +0100

    limits

--- Embedded_Dev/C++/Basics/data_type_limits.cpp
+++ Embedded_Dev/C++/Basics/data_type_limits.cpp
@@ -6,6 +6,14 @@ int main() {
     
     cout << "The upper limit of an int is: " << numeric_limits<int>::max() << endl;
     cout << "The lower limit of an int is: " << numeric_limits<int>::min() << endl;
+    cout << "The upper limit of an unsigned int is: " << numeric_limits<unsigned int>::max() << endl;
+    
+    int a = 2147483647;
+    int b = 2147483648;
+    
+    cout<<a<<endl;
+    cout<<b<<endl;
+    cout<<a+b<<endl;
 
     return 0;
 }
