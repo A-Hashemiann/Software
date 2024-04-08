73c99416e750c470bfcde3752d9d34650e3d99c5
A-Hashemiann <amen.hashemian@gmail.com>
2024-04-08 21:46:06 +0200

    arithmetic_overflow_underflow

--- Embedded_Dev/C++/Basics/arithmetic_overflow_underflow.cpp
+++ Embedded_Dev/C++/Basics/arithmetic_overflow_underflow.cpp
@@ -24,6 +24,23 @@ bool checkOverflowSub(int a, int b) {
     return false; // no overflow or underflow
 }
 
+// function to check for overflow during multiplication
+bool checkOverflowMul(int a, int b) {
+    if (a > 0 && b > 0 && a > numeric_limits<int>::max() / b) {
+        return true; // overflow will occur
+    }
+    if (a < 0 && b < 0 && a < numeric_limits<int>::min() / b) {
+        return true; // overflow will occur
+    }
+    if (a > 0 && b < 0 && a < numeric_limits<int>::min() / b) {
+        return true; // underflow will occur
+    }
+    if (a < 0 && b > 0 && a < numeric_limits<int>::max() / b) {
+        return true; // underflow will occur
+    }
+    return false; // no overflow or underflow
+}
+
 
 
 int main() {
